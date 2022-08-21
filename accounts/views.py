71ef06929221aac4d6c126498f0e88e568b983c5
RafsanRifat <rafsan.rifat255@gmail.com>
2022-08-21 16:55:39 +0600

    api running

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#Rest_Framework
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+
+@api_view(['GET', 'POST'])
+def students(request):
+    return Response({"message": "Hi, welcome to our system"})
