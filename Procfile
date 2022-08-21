9daebfa01437f127643dacc5cc17189ab743c021
RafsanRifat <rafsan.rifat255@gmail.com>
2022-08-21 19:42:56 +0600

    removed

--- Procfile
+++ Procfile
@@ -1,4 +1,2 @@
 web: gunicorn student_helper_backend.wsgi
-release: python manage.py makemigrations --noinput
-release: python manage.py collectstatic --noinput
 release: python manage.py migrate --noinput
--- student_helper_backend/settings.py
+++ student_helper_backend/settings.py
@@ -181,7 +181,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework'
+    'rest_framework',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -257,14 +258,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]  # new
+STATIC_ROOT = BASE_DIR / "staticfiles"  # new
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 django_heroku.settings(locals())
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
