73b9437180a6aba0d1788249e7f10692af214fc2
RafsanRifat <rafsan.rifat255@gmail.com>
2022-08-21 17:43:04 +0600

    env added

--- student_helper_backend/settings.py
+++ student_helper_backend/settings.py
@@ -9,17 +9,29 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+import os
+import environ
+from datetime import timedelta
 
+env = environ.Env(
+    # set casting, default value
+    SECRET_KEY=(str, ''),
+    EMAIL_HOST_USER=(str, ''),
+    EMAIL_HOST_PASSWORD=(str, ''),
+)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))  # Initialise environment variables
+ENV_VARIABLES = env
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yxwpnnvik=eacyqwv*imsob9lq2687cw(-sme7dscsx_r4!71l'
+SECRET_KEY = env('SECRET_KEY', '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
