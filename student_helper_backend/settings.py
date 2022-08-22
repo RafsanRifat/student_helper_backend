0acd5246217d2c8adfb79720bcdc3244462cb42c
RafsanRifat <rafsan.rifat255@gmail.com>
2022-08-22 10:30:04 +0600

    changed on settings

--- student_helper_backend/settings.py
+++ student_helper_backend/settings.py
@@ -167,7 +167,7 @@ ENV_VARIABLES = env
 SECRET_KEY = env('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
