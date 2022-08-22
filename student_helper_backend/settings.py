aa0d11a7e4321edbd06aedc4d5ad904810fad337
RafsanRifat <rafsan.rifat255@gmail.com>
2022-08-22 15:43:51 +0600

    djvhndjkv

--- student_helper_backend/settings.py
+++ student_helper_backend/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     # BASE_DIR / 'static',
 #     # BASE_DIR / 'quizes' / 'static',
@@ -136,7 +136,7 @@ STATIC_URL = '/staticfiles/'
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
